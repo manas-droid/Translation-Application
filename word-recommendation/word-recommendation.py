d8948319ec4cdd510bbd2c17c26b854fdc55845d
Manas Kalangan <manas.kalangan@gmail.com>
2024-12-30 20:26:29 -0500

    Added word recommendation api for german

---
+++ word-recommendation/word-recommendation.py
@@ -0,0 +1,36 @@
+import fasttext
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/api/words/suggestion', methods=['GET'])
+def word_suggestion():
+    lang = request.args.get('lang')
+    word = request.args.get('word')
+
+    try: 
+        print(request.args)
+        if lang == None or word == None or lang == '' or word == '':
+            raise AttributeError("Incorrect query parameter")
+
+        model = fasttext.load_model(f'cc.{lang}.300.bin')
+        related_words = model.get_nearest_neighbors(word, k=4)
+        related_words = [word for _, word in related_words]
+        return jsonify({
+            "related_words" : related_words
+        }), 200
+    except (FileNotFoundError, ValueError) :
+        return jsonify({"error": "Language not supported", "errorType":"LanguageNotSupport"}), 404
+    except AttributeError as ae:
+        return jsonify({"error": ae.args[0], "errorType":"IncorrectQuery"})
+    except Exception:
+        return jsonify({"error": "Something went wrong", "errorType": "Unknown"}), 400
+        
+
+
+
+
+
+if __name__ == '__main__':
+    # Specify the custom port here (e.g., 8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
